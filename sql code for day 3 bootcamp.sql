d835c123f9b5d5a2342527b5107cb84df3baba99
E PAVAN KUMAR <123948221+0pavankumar@users.noreply.github.com>
2023-08-10 08:27:28 +0530

    Add files via upload

---
+++ sql code for day 3 bootcamp.sql
@@ -0,0 +1,14 @@
+create table writers(title varchar(25), author varchar(30), published_year year);
+ insert into writers 
+ values("Harry Potter", "J.K. Rowling", 1997),
+("The Great Gatsby", "F. Scott Fitzgerald", 1925),
+("To Kill a Mockingbird", "Harper Lee", 1960);  
+
+select * from writers;
+
+
+
+
+
+
+ 
